3a88d34ff90c6d2a6b30d86d162053653dee28c2
Peteris Krumins <peteris.krumins@gmail.com>
2010-08-07 03:21:39 +0300

    sync example now uses encodeSync

--- tests/gif.js
+++ tests/gif.js
@@ -5,7 +5,7 @@ var Buffer = require('buffer').Buffer;
 
 var terminal = fs.readFileSync('./terminal.rgb');
 
-var gif = new Gif(terminal, 720, 400, 'rgb').encode();
+var gif = new Gif(terminal, 720, 400, 'rgb').encodeSync();
 
 fs.writeFileSync('./terminal.gif', gif, 'binary');
 
