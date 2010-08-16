f25446434e99003abd9b03d6124b95fc05915926
Peteris Krumins <peteris.krumins@gmail.com>
2010-08-16 21:35:10 +0300

    add filter_file_list declaration

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,7 @@
 char **find_files(const char *path);
 void free_file_list(char **file_list);
 int file_list_length(char **file_list);
+char **filter_file_list(char **file_list, int (*f)(const char *file));
 
 int file_size(const char *path);
 bool is_dir(const char *path);
